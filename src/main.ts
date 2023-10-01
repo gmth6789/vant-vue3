c45e293288341ad82c5a1d79dffdd923a55d6559
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-10-02 00:28:14 +0700

    3 files modified, 8 added, 1 deleted

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import pinia from './stores'
+import router from './router'
+// 样式全局使用
+import 'vant/lib/index.css'
+import './styles/main.scss'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(pinia)
+app.use(router)
+app.mount('#app')
---
+++ src/stores/index.ts
@@ -0,0 +1,12 @@
+import { createPinia } from 'pinia'
+import presist from 'pinia-plugin-persistedstate'
+
+const pinia = createPinia()
+pinia.use(presist)
+
+export default pinia
+
+// import { useUserStore } from './modules/user'
+// export { useUserStore }
+export * from './modules/user'
+export * from './modules/consult'
